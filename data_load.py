3ede1a2a06c487c4c65cb43b885d02a7b8d5014d
Koustubh2111 <koustubh.sudarshan@gmail.com>
2023-12-14 15:03:13 -0500

    fix query error

--- data_load.py
+++ data_load.py
@@ -21,34 +21,35 @@ db_connection = connect_MySQL.create_database_connection("localhost", "root", pw
 #%%
 #Query statment for creating the data scheme for credit risk taiwan data obtained from UCI-ML repo
 #Multi-line strings - triple quote notation
+#Make sure the variable names are not in double quotes when the query in triple quote notation
 create_table_query = """
 CREATE TABLE creditrisk 
 (
-    "ID"	INT,
-    "LIMIT_BAL"	INT,
-    "SEX"	VARCHAR(512),
-    "EDUCATION"	INT,
-    "MARRIAGE"	INT,
-    "AGE"	INT,
-    "PAY_0"	VARCHAR(512),
-    "PAY_2"	VARCHAR(512),
-    "PAY_3"	VARCHAR(512),
-    "PAY_4"	VARCHAR(512),
-    "PAY_5"	VARCHAR(512),
-    "PAY_6"	VARCHAR(512),
-    "BILL_AMT1"	INT,
-    "BILL_AMT2"	INT,
-    "BILL_AMT3"	INT,
-    "BILL_AMT4"	INT,
-    "BILL_AMT5"	INT,
-    "BILL_AMT6"	INT,
-    "PAY_AMT1"	INT,
-    "PAY_AMT2"	INT,
-    "PAY_AMT3"	INT,
-    "PAY_AMT4"	INT,
-    "PAY_AMT5"	INT,
-    "PAY_AMT6"	INT,
-    "default.payment.next.month"	VARCHAR(512)
+    ID INT,
+    LIMIT_BAL INT,
+    SEX VARCHAR(512),
+    EDUCATION INT,
+    MARRIAGE INT,
+    AGE INT,
+    PAY_0 VARCHAR(512),
+    PAY_2 VARCHAR(512),
+    PAY_3 VARCHAR(512),
+    PAY_4 VARCHAR(512),
+    PAY_5 VARCHAR(512),
+    PAY_6 VARCHAR(512),
+    BILL_AMT1 INT,
+    BILL_AMT2 INT,
+    BILL_AMT3 INT,
+    BILL_AMT4 INT,
+    BILL_AMT5 INT,
+    BILL_AMT6 INT,
+    PAY_AMT1 INT,
+    PAY_AMT2 INT,
+    PAY_AMT3 INT,
+    PAY_AMT4 INT,
+    PAY_AMT5 INT,
+    PAY_AMT6 INT,
+    DEF_PAY_NEXT_MON VARCHAR(512)
 );
 """
 #%%
