a85e56e84bb59d956a735ba5acde2d735ca2e2f1
Koustubh2111 <koustubh.sudarshan@gmail.com>
2023-12-15 15:40:58 -0500

    updated

--- data_eda.py
+++ data_eda.py
@@ -1,1 +1,19 @@
 #This file will extract the data and perform a basic EDA
+#%%
+import pandas as pd
+import connect_MySQL
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#%%
+#Connect to my SQL database and read the data into a pandas dataframe 
+#JOB : Pandas prefers SQLAlchemy engine or Database string URL or sqlite3 DBAPI connections 
+pw = "######"
+db = "credit_risk_taiwan"
+db_connection = connect_MySQL.create_database_connection("localhost", "root", pw, db)
+query = 'SELECT * FROM creditrisk'
+df = pd.read_sql(query, db_connection)
+# %%
+#Understanding the categorical variables
+
+#
