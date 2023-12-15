920c0dffaeec13b4637422e8ceb7169a2a1e2ac7
Koustubh2111 <koustubh.sudarshan@gmail.com>
2023-12-15 18:16:13 -0500

    bug fix

--- data_cleaning_eda.py
+++ data_cleaning_eda.py
@@ -85,19 +85,3 @@ def get_clean_data(df):
 
     return df
 
-'''
-plot_vars(['SEX', 'EDUCATION', 'MARRIAGE'], 'cat')
-
-plot_vars(['PAY_0', 'PAY_2', 'PAY_3', 'PAY_4', \
-           'PAY_5', 'PAY_6'], type='num')
-
-plot_vars(['BILL_AMT1', 'BILL_AMT2','BILL_AMT3', 'BILL_AMT4',\
-            'BILL_AMT5', 'BILL_AMT6'], type='num')
-
-plot_vars(['PAY_AMT1','PAY_AMT2', 'PAY_AMT3', \
-           'PAY_AMT4', 'PAY_AMT5', 'PAY_AMT6'], type='num')
-
-#Plot Output
-df.groupby('DEF_PAY_NEXT_MON').size().plot(kind='bar')
-'''
-
