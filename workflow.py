724d435886c90cffb1f57bf65f1e36f856f48c8f
Koustubh2111 <koustubh.sudarshan@gmail.com>
2023-12-18 21:55:48 -0500

    workflow

--- workflow.py
+++ workflow.py
@@ -1,1 +1,12 @@
 #This code will build a workflow for the project using prefect
+from prefect import task, flow
+import data_cleaning_eda
+import pandas as pd
+
+#Extracting the data from the SQL server
+@task
+def load_data(pw,db):
+    return data_cleaning_eda.get_data(pw,db)
+
+#
+  
